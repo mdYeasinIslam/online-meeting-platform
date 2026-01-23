a27d384a3cfc42ade2fdd984d0d80b18fb1c9140
mdYeasinIslam <hyeasinislam@gmail.com>
2026-01-24 01:45:27 +0600

    add cn utils function for optimize className

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import type { FormProps } from "antd";
-import { Button, Form, Input } from "antd";
+import { Button, Col, Form, Input, Row } from "antd";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 interface FieldType {
   username?: string;
   password?: string;
@@ -10,6 +11,7 @@ interface FieldType {
 }
 
 const page = () => {
+  const [authTab, setAuthTab] = useState("signUp");
   const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
     console.log("Success:", values);
   };
@@ -26,7 +28,7 @@ const page = () => {
           <h1 className="text-3xl font-semibold">Authentication System</h1>
           <Link href={"/"}>Back Home</Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-10">
+        <div className=" grid grid-cols-1 md:grid-cols-2 gap-10 mt-10">
           <div>
             <Image
               src={
@@ -38,60 +40,94 @@ const page = () => {
               className="w-full h-full"
             />
           </div>
-          <div className="">
+          <div className="max-w-lg mx-auto h-fit bg-[#07140D] p-6! rounded-3xl border border-[#084d2c]">
+            <div className="flex justify-between items-center">
+              <div className="mt-6">
+                <h1 className="uppercase text-kg font-semibold text-[#084d2c]">
+                  Credentials
+                </h1>
+                <p className="text-xl font-semibold">
+                  {authTab === "signIn"
+                    ? "Sign in to your account"
+                    : "Create a new account"}
+                </p>
+              </div>
+              <div className="flex justify-center items-center gap-4 bg-[#1B251F] rounded-full p-2">
+                <button
+                  onClick={() => setAuthTab("signIn")}
+                  className={`px-6 py-1 border-2  rounded-3xl ${authTab === "signIn" ? "border-green-500" : "border-gray-500"}`}
+                >
+                  Sign In
+                </button>
+                <button
+                  onClick={() => setAuthTab("signUp")}
+                  className={`px-6 py-1 border-2  rounded-3xl ${authTab === "signUp" ? "border-green-500" : "border-gray-500"}`}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
             <Form
-              name="basic"
-              labelCol={{ span: 8 }}
-              wrapperCol={{ span: 16 }}
-              style={{ maxWidth: 600 }}
+              name="sign-in"
               initialValues={{ remember: true }}
               onFinish={onFinish}
               onFinishFailed={onFinishFailed}
               autoComplete="off"
-              className="bg-[#07140D] p-6! rounded-xl"
+              className=""
               layout="vertical"
             >
-              <Form.Item
-                label="Email"
-                name="email"
-                rules={[
-                  { required: true, message: "Please input your email!" },
-                ]}
-              >
-                <Input
-                  className="bg-[#0B1A15]! hover:border-[#0EC971]! focus:border-[#0EC971]!"
-                  placeholder="Your email"
-                  size="large"
-                />
-              </Form.Item>
+              <Row gutter={16}>
+                <Col sm={24}>
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                      { required: true, message: "Please input your email!" },
+                    ]}
+                    className=""
+                  >
+                    <Input
+                      className="w-full! bg-[#0B1A15]! hover:border-[#0EC971]! focus:border-[#0EC971]! rounded-xl!"
+                      placeholder="Your email"
+                      size="large"
+                    />
+                  </Form.Item>
+                </Col>
+                <Col sm={24}>
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                    className=""
+                  >
+                    <Input.Password
+                      className="bg-[#0B1A15]! hover:border-[#0EC971]! focus-within:border-[#0EC971]! rounded-xl!"
+                      placeholder="Your password"
+                      size="large"
+                    />
+                  </Form.Item>
+                </Col>
 
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  { required: true, message: "Please input your password!" },
-                ]}
-              >
-                <Input.Password
-                  className="bg-[#0B1A15]! hover:border-[#0EC971]! focus-within:border-[#0EC971]!"
-                  placeholder="Your password"
-                  size="large"
-                />
-              </Form.Item>
-
-              {/* <Form.Item name="remember" valuePropName="checked">
+                {/* <Form.Item name="remember" valuePropName="checked">
                 <Checkbox>Remember me</Checkbox>
               </Form.Item> */}
-
-              <Form.Item>
-                <Button
-                  type="primary"
-                  className="bg-[#0EC971]!"
-                  htmlType="submit"
-                >
-                  Submit
-                </Button>
-              </Form.Item>
+                <Col sm={24}>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      className="w-full bg-[#0EC971]! "
+                      htmlType="submit"
+                    >
+                      Submit
+                    </Button>
+                  </Form.Item>
+                </Col>
+              </Row>
             </Form>
           </div>
         </div>
